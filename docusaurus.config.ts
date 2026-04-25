200e0d0555904ba2201d18a9c7216a1edcae8784
sawarame <yasushi@sawara.me>
2026-04-25 17:43:46 +0900

    fix: restructure markdown configuration for clarity

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -28,7 +28,13 @@ const config: Config = {
   projectName: 'sawarame.github.com', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    format: 'mdx',
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
