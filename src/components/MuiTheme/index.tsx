c9148fe5c25cf0ffadf4b0bba8253545c419a827
sawarame <yasushi@sawara.me>
2026-05-16 12:31:21 +0900

    feat: add @types/piexifjs for TypeScript support

--- src/components/MuiTheme/index.tsx
+++ src/components/MuiTheme/index.tsx
@@ -1,14 +1,19 @@
+import React from 'react';
 import { useColorMode } from '@docusaurus/theme-common';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 const muiLightTheme = createTheme({ palette: { mode: 'light' } });
 const muiDarkTheme = createTheme({ palette: { mode: 'dark' } });
+
+interface Props {
+  children: React.ReactNode;
+}
 
 /**
  * DinosaursのテーマとMUIのテーマカラーを合わせる.
  * @param param
  * @returns 
  */
-export default function MuiTheme({children}): JSX.Element {
+export default function MuiTheme({ children }: Props): JSX.Element {
   const { colorMode } = useColorMode();
   const muiTheme = colorMode === 'dark' ? muiDarkTheme : muiLightTheme;
 
